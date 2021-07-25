2f37940f302a986686266622db759845fbf36bf9
Arielst11 <asancheztroz@gmail.com>
2021-07-24 21:18:25 -0600

    se programan los ataques a la CPU y cambios en los tableros
    se programaron los ataques del usuario a la CPU y las actualizaciones que estos causan a los mapas, el menu de opciones solo ocupa configurar la tabla ranking , falta arreglar el while que termina el juego (este esta comentado para que funcione el codigo)

--- Proyecto/src/main/java/proyecto/Menu.java
+++ Proyecto/src/main/java/proyecto/Menu.java
@@ -16,7 +16,8 @@ public class Menu {
     
 public int coordenadaX;
 public int coordenadaY;    
-    
+
+
     
     
     
@@ -49,9 +50,71 @@ public int coordenadaY;
 // opciones del menu 
     public void opcionesMenu(){
        JOptionPane.showMessageDialog(null, "ese es el menu de opciones, tiene que escribir el numero de la opcion que quiere hacer");
-       JOptionPane.showMessageDialog(null, "opciones: 1 para atacar , 2 para ver tablero de ataque, 3 ver tablero propio, 4 hacer trampa, 5 mostrar marcador, 6 ver opciones otra vez.");
+       JOptionPane.showMessageDialog(null, "opciones: 1 para atacar , 2 para ver su tablero, 3 para hacer trampa, 4 para ver tablero de ataque, 5 mostrar marcador, 6 ver opciones otra vez.");
     
     }
+
+public void ataqueCoordenadaX(String letra){
+    int coordenadaX;
+       if (letra.equals("A")) {
+      coordenadaX = 1;
+      this.coordenadaX = coordenadaX;
+       }
+       if (letra.equals("B")) {     
+      coordenadaX = 2;  
+      this.coordenadaX = coordenadaX;
+       }
+      if (letra.equals("C")) {     
+      coordenadaX = 3; 
+      this.coordenadaX = coordenadaX;
+       }
+       if (letra.equals("D")) { 
+       coordenadaX = 4;
+       this.coordenadaX = coordenadaX;
+       }
+       if (letra.equals("E")) {    
+      coordenadaX = 5;
+      this.coordenadaX = coordenadaX;
+       }
+       if (letra.equals("F")) {
+      coordenadaX = 6;   
+      this.coordenadaX = coordenadaX;
+       }
+   JOptionPane.showMessageDialog(null, "Usted selecciono la fila "+ letra);
+       
+   }
+
+public void ataqueCoordenadaY (String numero){
+         int coordenadaY;
+         if(numero.equals("1")){
+             coordenadaY = 1;
+             this.coordenadaY = coordenadaY;
+              } 
+           if(numero.equals("2")){
+             coordenadaY = 2;
+             this.coordenadaY = coordenadaY;
+              } 
+           if(numero.equals("3")){
+             coordenadaY = 3;
+             this.coordenadaY = coordenadaY;
+              } 
+           if(numero.equals("4")){
+             coordenadaY = 4;
+             this.coordenadaY = coordenadaY;
+              } 
+           if(numero.equals("5")){
+             coordenadaY = 5;
+             this.coordenadaY = coordenadaY;
+              } 
+       if(numero.equals("6")){
+             coordenadaY = 6;        
+             this.coordenadaY = coordenadaY;
+              } 
+  JOptionPane.showMessageDialog(null, "Usted selecciono la columna "+ numero);
+   
+   }
+
+
 
 }
 
--- Proyecto/src/main/java/proyecto/NewMain.java
+++ Proyecto/src/main/java/proyecto/NewMain.java
@@ -22,20 +22,20 @@ public class NewMain {
        // se crean los barcos mediante el constructor para el jugador
        
        Barco jugadorAlmirante1 = new Barco("a",4);
-       Barco jugadorCapitan1 = new Barco("c",3);
-       Barco jugadorCapitan2 = new Barco("c",3);
-       Barco jugadorTeniente1 = new Barco("t",1);
-       Barco jugadorTeniente2 = new Barco("t",1);
-       Barco jugadorTeniente3 = new Barco("t",1);
+       Barco jugadorCapitan1 = new Barco("c1",3);
+       Barco jugadorCapitan2 = new Barco("c2",3);
+       Barco jugadorTeniente1 = new Barco("t1",1);
+       Barco jugadorTeniente2 = new Barco("t2",1);
+       Barco jugadorTeniente3 = new Barco("t3",1);
          
         // se crean los barcos mediante el constructor para el CPU 
          
        Barco cpuAlmirante1 = new Barco("a",4);
-       Barco cpuCapitan1 = new Barco("c",3);
-       Barco cpuCapitan2 = new Barco("c",3);
-       Barco cpuTeniente1 = new Barco("t",1);
-       Barco cpuTeniente2 = new Barco("t",1);
-       Barco cpuTeniente3 = new Barco("t",1);
+       Barco cpuCapitan1 = new Barco("c1",3);
+       Barco cpuCapitan2 = new Barco("c2",3);
+       Barco cpuTeniente1 = new Barco("t1",1);
+       Barco cpuTeniente2 = new Barco("t2",1);
+       Barco cpuTeniente3 = new Barco("t3",1);
         
         
         
@@ -158,15 +158,15 @@ public class NewMain {
        tableroCPU.creacionFilaColumna(tableroCPU1);
        
          // coordenadas del barco Almirante1 (del CPU)
-         tableroCPU.ingresaCoordenadasXYTablero(tableroCPU1, 1, 1, cpuAlmirante1.tipo);
+         tableroCPU.ingresaCoordenadasXYTablero(tableroCPU1, 1, 6, cpuAlmirante1.tipo);
          // coordenadas del barco Capitan1 (del CPU)
-         tableroCPU.ingresaCoordenadasXYTablero(tableroCPU1, 2, 2, cpuCapitan1.tipo);
+         tableroCPU.ingresaCoordenadasXYTablero(tableroCPU1, 2, 6, cpuCapitan1.tipo);
          // coordenadas del barco Capitan2 (del CPU)
-         tableroCPU.ingresaCoordenadasXYTablero(tableroCPU1, 3, 3, cpuCapitan2.tipo);
+         tableroCPU.ingresaCoordenadasXYTablero(tableroCPU1, 3, 6, cpuCapitan2.tipo);
          // coordenadas del barco Teniente1 (del CPU)
-         tableroCPU.ingresaCoordenadasXYTablero(tableroCPU1, 4, 4, cpuTeniente1.tipo);
+         tableroCPU.ingresaCoordenadasXYTablero(tableroCPU1, 4, 6, cpuTeniente1.tipo);
          // coordenadas del barco Teniente2 (del CPU)
-         tableroCPU.ingresaCoordenadasXYTablero(tableroCPU1, 5, 5, cpuTeniente2.tipo);
+         tableroCPU.ingresaCoordenadasXYTablero(tableroCPU1, 5, 6, cpuTeniente2.tipo);
          // coordenadas del barco Teniente3 (del CPU)
          tableroCPU.ingresaCoordenadasXYTablero(tableroCPU1, 6, 6, cpuTeniente3.tipo);
          
@@ -174,37 +174,174 @@ public class NewMain {
          
         
         JOptionPane.showMessageDialog(null, "este es el tablero de su oponente"); // !!!!!!!!!!!!!!!!!!!!!!!!!!! Eliminar esta linea de codigo solo es para testear el mapa del CPU
-         tableroCPU.mostrarMapa(tableroCPU1); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Eliminar esta linea de codigo solo es para testear el mapa del CPU
-
-      
-        /* este es el menu de opciones que tiene el jugador, meter todo esto como un metodo y llamarlo cada vez que el jugador tiene el turno. (falta completar las opciones), el turno del jugador termina solo
-       si selecciona el caso 1.*/
-        
-        
+        tableroCPU.mostrarMapa(tableroCPU1); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Eliminar esta linea de codigo solo es para testear el mapa del CPU
+         
+        // se crea el tablero de ataque.
+         Tablero tableroAtaque = new Tablero();          
+         String tableroAtaque1 [][] = new String [7][7];
+         tableroAtaque.creacionMarTablero(tableroAtaque1);
+         tableroAtaque.creacionFilaColumna(tableroAtaque1);
+         
+                 
+         
         menu1.opcionesMenu();
+       
+        int barcosJugador = 6;
+        int barcosCPU = 6;
+        int finalizadorJuego = 0; 
+       
+       while (finalizadorJuego == 0){
+            
+        /* if(barcosJugador = 0 || barcosCPU = 0 ){
+            finalizadorJuego = 1;
+        }
+        */    
        int terminadorTurno = 1;
        while (terminadorTurno == 1){
-       int seleccion = Integer.parseInt(JOptionPane.showInputDialog("Escriba el numero de la opcion que quiere realizar"));
+       int seleccion = Integer.parseInt(JOptionPane.showInputDialog("Este es su turno, escriba el numero de la opcion que quiere realizar"));
         switch (seleccion){
             
                 
-            // programar los ataques cuando se tenga listo el tablero del usuario y del CPU!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          
             case 1: 
                 
+             // esta opcion es para atacar.   
+             // se selecciona la coorenada X y se guarda en el menu1
+             String ataqueX = JOptionPane.showInputDialog("ingrese la coordenada de la fila (letra) donde quiere atacar");
+             menu1.ataqueCoordenadaX(ataqueX);
+             // se selecciona la coordenada Y y se guarda en el menu1
+             String ataqueY = JOptionPane.showInputDialog("ingrese la coordenada de la columna (numero) donde quiere atacar");
+             menu1.ataqueCoordenadaY(ataqueY);
+              
+             
+             // en caso de atacar un espacio sin barco
+             if(tableroCPU1[menu1.coordenadaX][menu1.coordenadaY].equals("=")){
+               tableroCPU1[menu1.coordenadaX][menu1.coordenadaY] = "0";
+               tableroAtaque1[menu1.coordenadaX][menu1.coordenadaY] = "0"; 
+               JOptionPane.showMessageDialog(null,"su ataque fallo, su ataque cayo en el mar");        
+             }
+             //En caso de atacar un espacio que ya se habia atacado sin barco
+              if(tableroCPU1[menu1.coordenadaX][menu1.coordenadaY].equals("0")){
+               tableroCPU1[menu1.coordenadaX][menu1.coordenadaY] = "0";
+               tableroAtaque1[menu1.coordenadaX][menu1.coordenadaY] = "0"; 
+               JOptionPane.showMessageDialog(null,"su ataque fallo, ustd ya habia atacado este punto");        
+             }
+             //En caso de atacar un espacio que ya tenia un barco destruido.
+             if(tableroCPU1[menu1.coordenadaX][menu1.coordenadaY].equals("X")){
+               tableroCPU1[menu1.coordenadaX][menu1.coordenadaY] = "X";
+               tableroAtaque1[menu1.coordenadaX][menu1.coordenadaY] = "X"; 
+               JOptionPane.showMessageDialog(null,"su ataque fallo, ustd ya habia destruido este barco");        
+             }
+             
+             // en caso de atacar al Almirante1
+             if(tableroCPU1[menu1.coordenadaX][menu1.coordenadaY].equals("a")){
+               cpuAlmirante1.vida = cpuAlmirante1.vida - 1;
+               JOptionPane.showMessageDialog(null, "ataque exitoso, usted ataque el barco Almirante1, a este barco le queda " +cpuAlmirante1.vida + " de vida" );
+               
+               tableroAtaque1[menu1.coordenadaX][menu1.coordenadaY] = "*";
+            // en caso de destruir el Almirante1
+               if(jugadorAlmirante1.vida == 0){
+                  barcosCPU = barcosCPU - 1;
+                JOptionPane.showMessageDialog(null, "Usted undio el barco Almirante del jugador " + CPU1.getNombre());
+                tableroCPU1[menu1.coordenadaX][menu1.coordenadaY] = "X";
+               tableroAtaque1[menu1.coordenadaX][menu1.coordenadaY] = "X";         
+               }
+               }
+             // en caso de atacar al Capitan1
+             if(tableroCPU1[menu1.coordenadaX][menu1.coordenadaY].equals("c1")){
+               cpuCapitan1.vida = cpuCapitan1.vida - 1;
+               JOptionPane.showMessageDialog(null, "ataque exitoso, usted ataque el barco Capitan1, a este barco le queda " +cpuCapitan1.vida + " de vida" );
+               
+               tableroAtaque1[menu1.coordenadaX][menu1.coordenadaY] = "*";
+            // en caso de destruir el Capitan1
+               if(jugadorCapitan1.vida == 0){
+                  barcosCPU = barcosCPU - 1;
+                JOptionPane.showMessageDialog(null, "Usted undio el barco Capitan1 del jugador " + CPU1.getNombre());
+                tableroCPU1[menu1.coordenadaX][menu1.coordenadaY] = "X";
+               tableroAtaque1[menu1.coordenadaX][menu1.coordenadaY] = "X";         
+               }
+               }
+              // en caso de atacar al Capitan2
+             if(tableroCPU1[menu1.coordenadaX][menu1.coordenadaY].equals("c2")){
+               cpuCapitan2.vida = cpuCapitan2.vida - 1; 
+               JOptionPane.showMessageDialog(null, "ataque exitoso, usted ataque el barco Capitan2, a este barco le queda " +cpuCapitan2.vida + " de vida" );
+             
+               tableroAtaque1[menu1.coordenadaX][menu1.coordenadaY] = "*";
+            // en caso de destruir el Capitan2
+               if(jugadorCapitan2.vida == 0){
+                  barcosCPU = barcosCPU - 1;
+                JOptionPane.showMessageDialog(null, "Usted undio el barco Capitan2 del jugador " + CPU1.getNombre());
+                tableroCPU1[menu1.coordenadaX][menu1.coordenadaY] = "X";
+               tableroAtaque1[menu1.coordenadaX][menu1.coordenadaY] = "X";         
+               }
+               }
+              // en caso de atacar al Teniente1
+             if(tableroCPU1[menu1.coordenadaX][menu1.coordenadaY].equals("t1")){
+               cpuTeniente1.vida = cpuTeniente1.vida - 1;
+               JOptionPane.showMessageDialog(null, "ataque exitoso, usted ataque el barco Teniente1, a este barco le queda " +cpuTeniente1.vida + " de vida" );
+           
+               tableroAtaque1[menu1.coordenadaX][menu1.coordenadaY] = "*";
+            // en caso de destruir el Teniente1
+               if(cpuTeniente1.vida == 0){
+                  barcosCPU = barcosCPU - 1;
+                JOptionPane.showMessageDialog(null, "Usted undio el barco Teniente1 del jugador " + CPU1.getNombre());
+                tableroCPU1[menu1.coordenadaX][menu1.coordenadaY] = "X";
+               tableroAtaque1[menu1.coordenadaX][menu1.coordenadaY] = "X";         
+               }
+               }
+             // en caso de atacar al Teniente2
+             if(tableroCPU1[menu1.coordenadaX][menu1.coordenadaY].equals("t2")){
+               cpuTeniente2.vida = cpuTeniente2.vida - 1;  
+               JOptionPane.showMessageDialog(null, "ataque exitoso, usted ataque el barco Teniente2, a este barco le queda " +cpuTeniente2.vida + " de vida" );
+             
+               tableroAtaque1[menu1.coordenadaX][menu1.coordenadaY] = "*";
+            // en caso de destruir el Teniente2
+               if(cpuTeniente2.vida == 0){
+                  barcosCPU = barcosCPU - 1;
+                JOptionPane.showMessageDialog(null, "Usted undio el barco Teniente2 del jugador " + CPU1.getNombre());
+                tableroCPU1[menu1.coordenadaX][menu1.coordenadaY] = "X";
+               tableroAtaque1[menu1.coordenadaX][menu1.coordenadaY] = "X";         
+               }
+               }
+             // en caso de atacar al Teniente3
+             if(tableroCPU1[menu1.coordenadaX][menu1.coordenadaY].equals("t3")){
+               cpuTeniente3.vida = cpuTeniente3.vida - 1;
+               JOptionPane.showMessageDialog(null, "ataque exitoso, usted ataque el barco Teniente3, a este barco le queda " +cpuTeniente3.vida + " de vida" );
+      
+               tableroAtaque1[menu1.coordenadaX][menu1.coordenadaY] = "*";
+            // en caso de destruir el Teniente3
+               if(cpuTeniente3.vida == 0){
+                  barcosCPU = barcosCPU - 1;
+                JOptionPane.showMessageDialog(null, "Usted undio el barco Teniente3 del jugador " + CPU1.getNombre());
+                tableroCPU1[menu1.coordenadaX][menu1.coordenadaY] = "X";
+               tableroAtaque1[menu1.coordenadaX][menu1.coordenadaY] = "X";         
+               }
+               }
+             
+          
                 terminadorTurno = 2;
                 JOptionPane.showMessageDialog(null, "Su turno ha terminado, el jugador " + CPU1.getNombre()+ " va a atacar ");
                 break;
-           // programar el tablero de ataque !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            
             case 2:
+               // muestra el tablero propio del Usuario 
+               JOptionPane.showMessageDialog(null, "este es su tablero propio"); 
+              tableroPropio.mostrarMapa(tableroPropio1);
                 break;
-            //programar tablero del usuario (mostrar el tablero del usuario)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             case 3:
-                break;
-           // programar hacer trampa (mostrar el tablero de la CPU)!!!!!!!!!!!!!!!!!!!!!
+            // muestra tablero CPU Trampa
+                JOptionPane.showMessageDialog(null, "este es el tablero de su oponente");
+                tableroCPU.mostrarMapa(tableroCPU1);
+                 break;   
             case 4:
+            // muestra el tablero de ataque    
+              JOptionPane.showMessageDialog(null, "este es el tablero de ataque");
+                tableroAtaque.mostrarMapa(tableroAtaque1);
                 break;
-            // mostrar el marcador (barcos del usuario y barcos del CPU, destruidos, danados y nuevos) !!!!!!!!!!!!!!!!!!!!!!!!
+           
             case 5:
+             // mostrar el marcador (barcos del usuario y barcos del CPU, destruidos, danados y nuevos) !!!!!!!!!!!!!!!!!!!!!!!!    
+                
                 break;
             case 6:
                 menu1.opcionesMenu();
@@ -213,6 +350,21 @@ public class NewMain {
                 JOptionPane.showMessageDialog(null, "tiene que seleccionar un valor entre 1 - 6, el valor 6 le permite ver las opciones disponibles");
             
         } 
+       
+       ///programar los turnos del CPU aqui. (SOLO ATACAR ALEATORIAMENTE) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+       
+       
+       
        }
-    }  
-    } 
+    
+        
+        
+        
+        
+        
+        
+        
+        } //ESTE CIERRA EL WHILE DE TURNOS 
+    }
+}
+
